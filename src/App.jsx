51f12f3286b725f0b524e915223696804ddcc528
AliGadoo <aligado232@gmail.com>
2025-09-23 22:47:07 +0300

    implement Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-7xl mx-auto px-4 h-screen font-inter">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,45 @@
+import logo from "/logo.png";
+
+const Navbar = () => {
+  return (
+    <header className="flex items-center justify-between py-4">
+      <div className="flex items-center gap-1">
+        <img className="w-16 h-16" src={logo} alt="Recipe Finder LOGO" />
+        <h1 className="text-2xl font-extrabold text-green-800">
+          Recipe Finder
+        </h1>
+      </div>
+      <nav>
+        <ul className="flex gap-4 text-green-600">
+          <li>
+            <a
+              className="hover:text-green-800 transition-colors duration-200"
+              href="#"
+            >
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a
+              className="hover:text-green-800 transition-colors duration-200"
+              href="#"
+            >
+              Search Recipes
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:text-green-800 transition-colors duration-200"
+              href=""
+            >
+              Random Recipe
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
